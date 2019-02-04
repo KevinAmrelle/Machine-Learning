5715c1aff1916860afa60b712890130a591c9b2d
Kevin Amrelle <45995751+KevinAmrelle@users.noreply.github.com>
2019-02-04 12:08:46 -0500

    Add files via upload

--- H2O- Deep Learning and AutoML.R
+++ H2O- Deep Learning and AutoML.R
@@ -2,12 +2,10 @@
 #     FUN IN H2O                 #
 ##################################
 
-
-
 library(h2o)
-
 h2o.init()
 
+# Data Preparation ##################
 url <- "http://h2o-public-test-data.s3.amazonaws.com/smalldata/iris/iris_wheader.csv"
 iris <- h2o.importFile(url)
 
@@ -18,30 +16,19 @@ test <- parts[[2]]
 # Deep Learning ####################
 
 mDL <- h2o.deeplearning(1:4, 5, train)
-
 mDL
-
 p <- h2o.predict(mDL, test)
-
 p
-
 h2o.performance(mDL, test)
 
 # Auto ML Feature ###################
 
 nrow(train)
 nrow(test)
-
-
 mA <- h2o.automl(1:4, 5, train,
                  max_runtime_secs = 30)
-
 mA
-
 mA@leaderboard
-
 p <- h2o.predict(mA@leader, test)
-
 p
-
 h2o.performance(mA@leader, test)
--- H2O- Random Forest and Gradient Boosting.R
+++ H2O- Random Forest and Gradient Boosting.R
@@ -2,12 +2,10 @@
 #     FUN IN H2O                 #
 ##################################
 
-
-
 library(h2o)
-
 h2o.init()
 
+# Data Preparation ##################
 url <- "http://h2o-public-test-data.s3.amazonaws.com/smalldata/iris/iris_wheader.csv"
 iris <- h2o.importFile(url)
 
@@ -17,27 +15,17 @@ test <- parts[[2]]
 
 
 # Random Forest #######################
-
 mRF <- h2o.randomForest(1:4, 5, train)
-
 mRF
-
 #p <- h2o.predict(mRF, test)
-
 h2o.performance(mRF, test)
-
 ?h2o.randomForest #How to view the Random Forest model "Help Page"
 
 
 
 # Gradient Boosting Estimator ##########
-
 mGBM <- h2o.gbm(1:4, 5, train)
-
 mGBM
-
 #p <- h2o.predict(mGBM, test)
-
 h2o.performance(mGBM, test)
-
 ?h2o.gbm #How to view the gradient boosted classification or regression trees "Help Page"
