ad3eb6693f54a87207a70769ee7deb4910a3e48e
Kevin Amrelle <45995751+KevinAmrelle@users.noreply.github.com>
2019-02-04 11:59:07 -0500

    Add files via upload

---
+++ H2O- Random Forest and Gradient Boosting.R
@@ -0,0 +1,43 @@
+##################################
+#     FUN IN H2O                 #
+##################################
+
+
+
+library(h2o)
+
+h2o.init()
+
+url <- "http://h2o-public-test-data.s3.amazonaws.com/smalldata/iris/iris_wheader.csv"
+iris <- h2o.importFile(url)
+
+parts <- h2o.splitFrame(iris,0.8)
+train <- parts[[1]]
+test <- parts[[2]]
+
+
+# Random Forest #######################
+
+mRF <- h2o.randomForest(1:4, 5, train)
+
+mRF
+
+#p <- h2o.predict(mRF, test)
+
+h2o.performance(mRF, test)
+
+?h2o.randomForest #How to view the Random Forest model "Help Page"
+
+
+
+# Gradient Boosting Estimator ##########
+
+mGBM <- h2o.gbm(1:4, 5, train)
+
+mGBM
+
+#p <- h2o.predict(mGBM, test)
+
+h2o.performance(mGBM, test)
+
+?h2o.gbm #How to view the gradient boosted classification or regression trees "Help Page"
