ef86faa8de88a6c612b8724b0d192d5430535838
Kevin Amrelle <45995751+KevinAmrelle@users.noreply.github.com>
2019-02-04 12:24:33 -0500

    Add files via upload

--- H2O- Deep Learning and AutoML.R
+++ H2O- Deep Learning and AutoML.R
@@ -5,16 +5,16 @@
 library(h2o)
 h2o.init()
 
-# Data Preparation ##################
+# Pulling Iris H2O Dataset ##########
 url <- "http://h2o-public-test-data.s3.amazonaws.com/smalldata/iris/iris_wheader.csv"
 iris <- h2o.importFile(url)
 
+# Data Preparation ##################
 parts <- h2o.splitFrame(iris,0.8)
 train <- parts[[1]]
 test <- parts[[2]]
 
 # Deep Learning ####################
-
 mDL <- h2o.deeplearning(1:4, 5, train)
 mDL
 p <- h2o.predict(mDL, test)
@@ -22,7 +22,6 @@ p
 h2o.performance(mDL, test)
 
 # Auto ML Feature ###################
-
 nrow(train)
 nrow(test)
 mA <- h2o.automl(1:4, 5, train,
--- H2O- Random Forest and Gradient Boosting.R
+++ H2O- Random Forest and Gradient Boosting.R
@@ -5,10 +5,11 @@
 library(h2o)
 h2o.init()
 
-# Data Preparation ##################
+# Pulling Iris H2O Dataset ##########
 url <- "http://h2o-public-test-data.s3.amazonaws.com/smalldata/iris/iris_wheader.csv"
 iris <- h2o.importFile(url)
 
+# Data Preparation ##################
 parts <- h2o.splitFrame(iris,0.8)
 train <- parts[[1]]
 test <- parts[[2]]
@@ -28,4 +29,4 @@ mGBM <- h2o.gbm(1:4, 5, train)
 mGBM
 #p <- h2o.predict(mGBM, test)
 h2o.performance(mGBM, test)
-?h2o.gbm #How to view the gradient boosted classification or regression trees "Help Page"
+?h2o.gbm 
